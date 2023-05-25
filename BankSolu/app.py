a049305280c44ccf429f5dd39cbe2d54e7c0a75e
Fred <fredkesse1234@gmail.com>
2023-05-25 11:24:21 +0000

    ajout du requiement

--- BankSolu/app.py
+++ BankSolu/app.py
@@ -1,50 +1,29 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
+from classe import User
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///projet.db"
-db = SQLAlchemy(app)
-
-@app.route("/index")
-def accueil():
-
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form.get("nom")
+        prenom = request.form.get("prenom")
+        email = request.form.get("email")
+        num = request.form.get("num")
+        user = User()
+        user.add_student(name, prenom, email, num)
+        print(f"nom: {name}, prenom: {prenom}, email: {email}, num: {num}")
+        return redirect(url_for("user"))
     return render_template("index.html")
 
-@app.route("/incription")
-def ins():
+@app.route('/students')
+def student():
+    user = User()
+    data = user.get_all_user()
+    for i in data:
+        print(i)
+    return render_template("list.html", users=user.get_all_user())
 
-    return render_template("inscription.html")
-
-
-
-
-
-
-if __name__ == "__main__":
-    app.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    app.run()
+@app.route('/students/<int:id>')
+def get_user(id):
+    return f"""<h2>Je suis l'élève {id}: </h3>"""
---
+++ BankSolu/classe.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+conn = sqlite3.connect("../data/users.db")
+
+cur = conn.cursor()
+
+class User:
+
+    def __init__(self):
+        self.conn = sqlite3.connect("../data/users.db")
+        self.conn.row_factory = sqlite3.Row
+        self.cur = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS users(
+        id INTEGER  PRIMARY KEY,
+        nom TEXT,
+        prenom INTEGER,
+        email TEXT,
+        num INTERGER
+        )''')
+
+    def add_user(self, nom, prenom, email, num):
+        self.cur.execute("""INSERT INTO users(nom, prenom, email, num) VALUES(?, ?, ?, ?)""", (nom, prenom, email, num))
+        self.conn.commit()
+
+    def get_all_user(self):
+        self.cur.execute('''SELECT * FROM users''')
+        users = self.cur.fetchall()
+        return students 
+     
+    def get_user(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delate(self):
+        pass
+
