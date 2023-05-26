9456aaf906d219c89913c5bf428001be1399116a
Fred <fredkesse1234@gmail.com>
2023-05-26 08:36:41 +0000

    ajout de la base de données

--- BankSolu/app.py
+++ BankSolu/app.py
@@ -1,89 +1,51 @@
-from flask import Flask, render_template, request, redirect
-import sqlite3
-from users import UserDatabase
+from flask import Flask, render_template, request, redirect, url_for
+from users import User
 
 app = Flask(__name__)
 
-# Classe gérant la base de données des utilisateurs
-class UserDatabase:
-    def __init__(self, db_name):
-        self.conn = sqlite3.connect(db_name)
-        self.cursor = self.conn.cursor()
-        
-        # ...
-
-    def verify_credentials(self, email, password):
-        # Vérifier les informations d'identification de l'utilisateur dans la base de données
-        self.cursor.execute('''
-            SELECT * FROM users
-            WHERE email=? AND password=?
-        ''', (email, password))
-        row = self.cursor.fetchone()
-        
-        if row is None:
-            return False
-        
-        return True
-
-db = UserDatabase('users.db')
-
-# Route pour la page d'accueil
-@app.route('/', methods=['GET', 'POST'])
+db = User()
+# Home page route
+@app.route("/", methods=['GET', 'POST'])
 def accueil():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        
-        # Vérifier les informations d'identification dans la base de données
-        if db.verify_credentials(email, password):
-            # Informations d'identification valides, rediriger vers une autre page
-            return redirect('/dashboard')
-        
-        # Informations d'identification invalides, afficher un message d'erreur
-        return render_template('index.html', error=True)
-    
-    return render_template('index.html', error=False)
+    return render_template("index.html")
 
-# Route pour la page d'inscription
+# Registration route
 @app.route('/inscription', methods=['GET', 'POST'])
 def inscription():
     if request.method == 'POST':
         name = request.form['name']
         prenom = request.form['prenom']
         email = request.form['email']
+        mdp = request.form['mdp']
         phone = request.form['phone']
+        user = User()
+        try:
+            user.add_user(name,prenom,email, mdp,phone)
+            print("passs ooo")
+        except:
+            return render_template("inscription.html", error="Un utilisateur avec cet email existe déjà")
         
-        # Créer un nouvel utilisateur
-        user = UserDatabase(name=name, prenom=prenom, email=email, phone=phone)
-        
-        # Vérifier si l'utilisateur existe déjà dans la base de données
-        if not db.user_exists(user):
-            # Ajouter l'utilisateur dans la base de données
-            db.add_user(user)
-            
-        # Rediriger vers le tableau de bord
-        return redirect('/dashboard')
-    
+        return redirect(url_for('tableau'))
+    return render_template('inscription.html')
+
+@app.route('/show_login', methods=['POST', 'GET'])
+def show_login():
     return render_template('login.html')
 
-# Route pour la connexion de l'utilisateur
-@app.route('/connexion', methods=['POST'])
+
+# Login route
+@app.route('/connexion', methods=['POST', 'GET'])
 def connexion():
     email = request.form['email']
     password = request.form['password']
     
-    # Vérifier les informations d'identification dans la base de données
-    if db.verify_credentials(email, password):
-        # Informations d'identification valides, rediriger vers le tableau de bord
-        return redirect('/dashboard')
+    # Perform login verification here using the User class
     
-    # Informations d'identification invalides, afficher un message d'erreur
-    return render_template('inscription.html', error=True)
+    return redirect(url_for('index'))
 
-@app.route('/dashboard')
-def dashboard():
-    # Page du tableau de bord, accessible après une connexion réussie
-    return render_template('Tableau_bord.html')
+@app.route("/dashboard", methods=['GET', 'POST'])
+def tableau():
+    return render_template("Tableau_bord.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- BankSolu/users.py
+++ BankSolu/users.py
@@ -1,93 +1,80 @@
 import sqlite3
 
-conn = sqlite3.connect("./data/users.db")
 
-cur = conn.cursor()
-
-# Classe représentant un utilisateur
 class User:
-    def __init__(self, name, prenom, email, phone):
-        self.name = name
-        self.prenom = prenom
-        self.email = email
-        self.phone = phone
+    def __init__(self):
+        self.conn = sqlite3.connect("./users.db")
+        self.conn.row_factory = sqlite3.Row
+        self.cur = self.conn.cursor()
+        self.create_table()
 
-# Classe gérant la base de données des utilisateurs
-class UserDatabase:
-    def __init__(self, db_name):
-        self.conn = sqlite3.connect(db_name)
-        self.cursor = self.conn.cursor()
-        
-        # Créer la table "users" si elle n'existe pas déjà
-        self.cursor.execute('''
+    def create_table(self):
+        self.cur.execute('''
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL,
                 prenom TEXT NOT NULL,
                 email TEXT NOT NULL,
+                mdp TEXT NOT NULL,
                 phone TEXT NOT NULL
             )
         ''')
         self.conn.commit()
-    
-    def add_user(self, user):
-        # Insérer un nouvel utilisateur dans la base de données
-        self.cursor.execute('''
-            INSERT INTO users (name, prenom, email, phone)
-            VALUES (?, ?, ?, ?)
-        ''', (user.name, user.prenom, user.email, user.phone))
-        self.conn.commit()
-    
+
+    def add_user(self, name, prenom, email, mdp, phone):
+        try:
+            self.cur.execute('''
+                INSERT INTO users(name, prenom, email, mdp, phone)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (name, prenom, email, mdp, phone))
+            self.conn.commit()
+            print("User enregistrer")
+            return "User enregistrer"
+        except:
+            print("Error")
+            return "Error"
+
+    def get_all_users(self):
+        self.cur.execute('SELECT * FROM users')
+        rows = self.cur.fetchall()
+
+        users = []
+        for row in rows:
+            user = {
+                'id': row['id'],
+                'name': row['name'],
+                'prenom': row['prenom'],
+                'email': row['email'],
+                'phone': row['phone']
+            }
+            users.append(user)
+
+        return users
+
+    def get_user(self, user_id):
+        self.cur.execute('SELECT * FROM users WHERE id=?', (user_id,))
+        row = self.cur.fetchone()
+
+        if row:
+            user = {
+                'id': row['id'],
+                'name': row['name'],
+                'prenom': row['prenom'],
+                'email': row['email'],
+                'phone': row['phone']
+            }
+            return user
+
+        return None
+
     def update_user(self, user_id, new_user):
-        # Mettre à jour les informations d'un utilisateur dans la base de données
-        self.cursor.execute('''
+        self.cur.execute('''
             UPDATE users
             SET name=?, prenom=?, email=?, phone=?
             WHERE id=?
-        ''', (new_user.name, new_user.prenom, new_user.email, new_user.phone, user_id))
+        ''', (new_user['name'], new_user['prenom'], new_user['email'], new_user['phone'], user_id))
         self.conn.commit()
-    
-    def delete_user(self, user_id):
-        # Supprimer un utilisateur de la base de données
-        self.cursor.execute('DELETE FROM users WHERE id=?', (user_id,))
-        self.conn.commit()
-    
-    def get_all_users(self):
-        # Récupérer tous les utilisateurs de la base de données
-        self.cursor.execute('SELECT * FROM users')
-        rows = self.cursor.fetchall()
-        
-        users = []
-        for row in rows:
-            user = User(row[1], row[2], row[3], row[4])
-            user.id = row[0]
-            users.append(user)
-        
-        return users
 
-# Exemple d'utilisation
-if __name__ == '__main__':
-    db = UserDatabase('users.db')
-    
-    # # Créer un utilisateur
-    # user1 = User('John', 'Doe', 'john@example.com', '123456789')
-    
-    # # Ajouter l'utilisateur à la base de données
-    # db.add_user(user1)
-    
-    # # Récupérer tous les utilisateurs de la base de données
-    # all_users = db.get_all_users()
-    # for user in all_users:
-    #     print(user.name, user.prenom, user.email, user.phone)
-    
-    # # Mettre à jour les informations d'un utilisateur
-    # user1.email = 'new_email@example.com'
-    # db.update_user(user1.id, user1)
-    
-    # # Supprimer un utilisateur
-    # db.delete_user(user1.id)
-    
-    # # Récupérer tous les utilisateurs mis à jour
-    # all_users = db.get_all_users()
-    # for user in all_users:
-    #     print(user.name, user.prenom, user.email, user.phone)
+    def delete_user(self, user_id):
+        self.cur.execute('DELETE FROM users WHERE id=?', (user_id,))
+        self.conn.commit()
